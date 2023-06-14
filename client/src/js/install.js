46e89fb6f7e2cba2ecd994f817312dae0bbf3b80
johnfitz3 <120284650+johnfitz3@users.noreply.github.com>
2023-06-14 16:04:45 -0400

    deploying to heroku

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -15,7 +15,7 @@ window.addEventListener('beforeinstallprompt', (event) => {
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
     const promptEvent = window.defferedPrompt;
-    if(!prompEvent){
+    if(!promptEvent){
         return;
     }
 
